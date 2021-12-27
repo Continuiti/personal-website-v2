a3e94ed7aae8e42abb579668fa110734128cbfdc
thienlongtran <tltran5@uno.edu>
2021-12-26 22:01:42 -0600

    Update Bio

---
+++ _includes/bio_code.py
@@ -0,0 +1,33 @@
+class Thien():
+    def __init__(self):
+        self.name = "Thien Tran"
+        self.home = "New Orleans, LA"
+        self.currentSchool = "Georgia Tech"
+        self.degreeProgram = "M.S. Computer Science"
+        self.expectedGraduation = "December 2023"
+        self.undergradSchool = "University of New Orleans"
+        self.hobbies = {
+            "Skateboarding",
+            "Biking",
+            "Watching TV Shows",
+            "Going to the Movies",
+            "Chilling"
+        }
+
+    def getInfo():
+        return {
+            Email("ttran384@gatech.edu"),
+            Github("thienlongtran"),
+            LinkedIn("thienlongtran"),
+            Resume("thienlongtran")
+        }
+
+    def getFunFacts():
+        return {
+            "favoriteClass" = "Astronomy",
+            "favoriteTVShow" = "Cosmos: A Spacetime Odyssey",
+            "favoriteMovie" = "Frozen II",
+            "favoriteSinger" = "Annapantsu",
+            "favoriteFood" = "Gumbo",
+            "favoriteAuthor" = "J.K. Rowling"
+        }
--- _includes/test.py
+++
@@ -1,7 +0,0 @@
-class Thien():
-    def __init__(self):
-        self.name = "Thien Tran"
-        self.currentSchool = "Georgia Tech"
-        self.degreeProgram = "M.S. Computer Science"
-        self.expectedGraduation = "December 2023"
-        self.undergradSchool = "University of New Orleans"
