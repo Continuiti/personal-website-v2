41f02a7f71523ae277536a7fd8f18c0f04569d4b
thienlongtran <tltran5@uno.edu>
2021-12-26 20:47:14 -0600

    Updated About Me layout

---
+++ _includes/test.py
@@ -0,0 +1,7 @@
+class Thien():
+    def __init__(self):
+        self.name = "Thien Tran"
+        self.currentSchool = "Georgia Tech"
+        self.degreeProgram = "M.S. Computer Science"
+        self.expectedGraduation = "December 2023"
+        self.undergradSchool = "University of New Orleans"
